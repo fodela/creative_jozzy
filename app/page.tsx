1ce7a960df01e296c52278f2a66878d3fdc95982
fodela <particleme30@gmail.com>
2024-05-10 04:32:36 +0000

    feat: add sections to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import FAQ from "@/components/FAQ";
+import Gallery from "@/components/Gallery";
 import Hero from "@/components/Hero";
+import ServicesSection from "@/components/ServicesSection";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
+      <ServicesSection />
+      <Gallery />
+      <Testimonials />
+      <FAQ />
     </main>
   );
 }
